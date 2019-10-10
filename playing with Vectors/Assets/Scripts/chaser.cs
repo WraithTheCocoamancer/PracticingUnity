e991edee333a1c1b6895bd442ab886a8b95a8543
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2019-10-10 13:42:41 +0100

    target and chaser
    vectors practice

--- playing with Vectors/Assets/Scripts/chaser.cs
+++ playing with Vectors/Assets/Scripts/chaser.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementTarget : MonoBehaviour
+public class chaser : MonoBehaviour
 {
+    public float speed = 5f;
+    public Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,9 @@ public class MovementTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0,-90,0), Space.Self);
+
+
     }
 }
---
+++ playing with Vectors/Assets/Scripts/target.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    void Start()
+    {
+        moveSpeed = 8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
